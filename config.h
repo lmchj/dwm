9814700a2b52337f6eb8862d7e7f6f3f00d942bb
arch-lm <lmchj@pm.me>
2020-07-06 18:45:44 -0600

    Floating rules for Galculator and R graphics. Fixed annoying gaps due to terminal columns and rows respect (don't respect terminal's will, they are insane).

--- config.h
+++ config.h
@@ -33,12 +33,14 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "R_x11",    NULL,       NULL,       0,       		1,           -1 },
+	{ "Galculator",    NULL,       NULL,       0,       		1,           -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
